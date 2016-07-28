a608db0dff35832507b5b58a73065e27db2fe35b
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-07-28 16:56:39 +0200

    Adapt file names and use relative path for instantiation.

---
+++ task2/MetaDataReader.cs
@@ -0,0 +1,19 @@
+﻿using iTextSharp.text.pdf;
+
+namespace task2
+{
+    internal class MetaDataReader
+    {
+        private readonly PdfReader _reader;
+
+        public MetaDataReader(string pdfPath)
+        {
+            _reader = new PdfReader(pdfPath);
+        }
+
+        public string ReadEntry(string name)
+        {
+            return null;
+        }
+    }
+}
---
+++ task2/tests/MetaDataReaderTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace task2.tests
+{
+    [TestClass]
+    public class MetaDataReaderTest
+    {
+        private MetaDataReader _sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _sut = new MetaDataReader("task2.pdf");
+        }
+
+        [TestMethod]
+        public void GetAuthor()
+        {
+            var actual = _sut.ReadEntry("Author");
+            var expected = "CUPS";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
